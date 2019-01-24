0efc856c7a2a2a4dd2cedb37fb31ded071e4c1c6
Nicolas Vigari <vigari.sistemas@gmail.com>
2019-01-23 22:17:13 -0300

    Symfony 3.2 + entities

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+	
+	   /**
+     * @var Product
+     *
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy = "product_id")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+	private $category_id;
+	function getCategory_id(): Product {
+		return $this->category_id;
+	}
+
+	function setCategory_id(Product $category_id) {
+		$this->category_id = $category_id;
+	}
+
+	
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Product
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
+
